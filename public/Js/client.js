70fe8a4e45d674489f8e803be5ac3e5848eae76a
simmytun <simmytun@gmail.com>
2020-11-07 11:48:57 -0700

    initial commit

---
+++ public/Js/client.js
@@ -0,0 +1,31 @@
+// import animals module
+
+
+import { animals } from './animal.js';
+
+
+// Create a `outputHTML` variable where you will store the html that will eventually be printed to the page
+
+
+let outputHTML = '';
+
+
+// Using `document.querySelector()`, assign the .gallery element to a `gallery` variable.
+
+
+const gallery = document.querySelector('.gallery');
+
+
+// Loop through `animals` array using `array.forEach()`. 
+
+
+animals.forEach(function (animal) {
+
+    outputHTML += `<figure class="card">
+      <img src = ${animal.path} alt = 'Lorem Picsum: ${animal.title}' ${Object.keys(animal)} = ${Object.values(animal)} ${Object.keys(animal)} = ${Object.values(animal)}>
+      <figcaption><h1>${animal.title}</h1><p>${animal.credit}</p><a href=${animal.url}><h4> Link</h4> </a></figcaption>
+     </figure>`;
+
+
+gallery.innerHTML = outputHTML;
+});
